eb495f9d0f31473d692d74c9a83fcca63d898766
Milo Hartsoe <milo.red.duck@gmail.com>
2017-01-03 03:06:48 +0000

    Add loader

--- public/home.js
+++ public/home.js
@@ -4,12 +4,22 @@ window.onload = function() {
     document.getElementById("basic-addon3 site").textContent = url;
 }
 
+var spinner = "<"
+
 function requestShrt() {
     $("#response-alert").removeClass('alert-danger');
     $("#response-alert").removeClass('alert-success');
+    $(".loader").css({
+        "visibility": "visible"
+    });
+    $("#response").text("");
     var long = $('#long-url').val();
     var short = $('#basic-url').val();
     if (!long) {
+
+        $(".loader").css({
+            "visibility": "hidden"
+        });
         $("#response-alert").addClass('alert-danger');
         $("#response").text('Please provide a url');
     }
@@ -25,6 +35,9 @@ function requestShrt() {
         $.ajax({
             url: url,
             success: function(result) {
+                $(".loader").css({
+                    "visibility": "hidden"
+                });
                 if (result.error) {
                     $("#response").text(result.error);
                     $("#response-alert").addClass('alert-danger');
@@ -38,7 +51,7 @@ function requestShrt() {
                 else {
                     var shortURL = result.short;
                     $("#response").text('The new short ' + shortURL + ' redirects to ' + result.original);
-                    $(".short-url").attr('href',shortURL).text(shortURL);
+                    $(".short-url").attr('href', shortURL).text(shortURL);
                     $("#response-alert").addClass('alert-success');
                     $("#long-url").val("");
                     $("#basic-url").val("");
